95b91bb9c5fd6339c62e431d8214ed5ecf3cba57
Vikas <57795868+VikasAmlaha@users.noreply.github.com>
2025-08-24 20:18:04 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React with Parcel 🚀</h1>
+      <p>This is my first professional React setup.</p>
+    </div>
+  );
+}
+
+export default App;
